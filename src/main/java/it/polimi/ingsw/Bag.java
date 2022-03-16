4494de0afb21caa8e9cfee719b8b9e6823dbff36
Benedart <49732385+Benedart@users.noreply.github.com>
2022-03-17 00:59:13 +0100

    Added drawStudent and Students' constructor

--- src/main/java/it/polimi/ingsw/Bag.java
+++ src/main/java/it/polimi/ingsw/Bag.java
@@ -3,13 +3,25 @@ package it.polimi.ingsw;
 import java.util.Random;
 
 public class Bag {
+    final static int MAX_STUDENTS = 126;
     final private Students students;
+    private Random rng;
 
     public Bag(Students students) {
         this.students = students;
     }
 
     public Color drawStudent(){
+        int size = students.values().stream().reduce(0, Integer::sum);
+        int color_index = rng.nextInt(size);
+
+        int count = 0;
+        for (Color color : students.keySet()) {
+            count += students.get(color);
+
+            if(color_index - count <= 0) return color;
+        }
+
         return null;
     }
 }
--- src/main/java/it/polimi/ingsw/Students.java
+++ src/main/java/it/polimi/ingsw/Students.java
@@ -2,4 +2,20 @@ package it.polimi.ingsw;
 
 import java.util.HashMap;
 
-public class Students extends HashMap<Color, Integer> {}
+public class Students extends HashMap<Color, Integer> {
+    public Students(int greens, int blues, int yellows, int pinks, int reds){
+        this.put(Color.GREEN, greens);
+        this.put(Color.BLUE, blues);
+        this.put(Color.YELLOW, yellows);
+        this.put(Color.PINK, pinks);
+        this.put(Color.RED, reds);
+    }
+
+    public Students(){
+        this.put(Color.GREEN, 0);
+        this.put(Color.BLUE, 0);
+        this.put(Color.YELLOW, 0);
+        this.put(Color.PINK, 0);
+        this.put(Color.RED, 0);
+    }
+}
