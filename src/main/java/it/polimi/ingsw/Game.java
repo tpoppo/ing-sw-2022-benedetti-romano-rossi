4a276d15b994a5f7867b770ed12fc6b63807af0d
Benedart <49732385+Benedart@users.noreply.github.com>
2022-03-17 22:30:22 +0100

    Added fillClouds

--- src/main/java/it/polimi/ingsw/Game.java
+++ src/main/java/it/polimi/ingsw/Game.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Queue;
 
 public abstract class Game {
+    final private int CLOUD_SPACE;
+
     final private boolean expert_mode;
     final private Player first_player;
     final private Queue<Player> play_order;
@@ -14,7 +16,8 @@ public abstract class Game {
     final private ArrayList<Character> characters;
     final private int inhibit_towers;
 
-    protected Game(boolean expert_mode, Player first_player, Queue<Player> play_order, ArrayList<Island> islands, Bag bag, ArrayList<Students> clouds, ArrayList<Player> players, ArrayList<Character> characters, boolean inhibit_towers, int inhibit_towers1) {
+    protected Game(int cloud_space, boolean expert_mode, Player first_player, Queue<Player> play_order, ArrayList<Island> islands, Bag bag, ArrayList<Students> clouds, ArrayList<Player> players, ArrayList<Character> characters, int inhibit_towers1, int inhibit_towers) {
+        CLOUD_SPACE = cloud_space;
         this.expert_mode = expert_mode;
         this.first_player = first_player;
         this.play_order = play_order;
@@ -23,14 +26,22 @@ public abstract class Game {
         this.clouds = clouds;
         this.players = players;
         this.characters = characters;
-        this.inhibit_towers = inhibit_towers1;
+        this.inhibit_towers = inhibit_towers;
     }
 
     public static Game setup(int num_players, boolean expert_mode){ return null; }
 
     public void nextTurn(){}
 
-    public void fillClouds(){}
+    public void fillClouds(){
+        for (Students cloud : clouds) {
+            for (int i=0; i < CLOUD_SPACE; i++) {
+                Color drawnColor = bag.drawStudent();
+
+                cloud.put(drawnColor, cloud.get(drawnColor) + 1);
+            }
+        }
+    }
 
     public void beginPlanning(){}
 
