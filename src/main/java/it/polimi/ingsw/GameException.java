4ec9436d2e5cdd955d49ff855f2098c3fbfe5a4b
Lorenzo Rossi <30434380+tpoppo@users.noreply.github.com>
2022-03-17 22:55:27 +0100

    Added GameException' constructor

--- src/main/java/it/polimi/ingsw/GameException.java
+++ src/main/java/it/polimi/ingsw/GameException.java
@@ -1,4 +1,13 @@
 package it.polimi.ingsw;
 
 public class GameException extends Exception{
+    final private String info;
+    GameException(String info){
+        this.info = info;
+    }
+
+    @Override
+    public String toString() {
+        return "GameException("+this.info+")";
+    }
 }
