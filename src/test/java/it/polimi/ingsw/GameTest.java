8a2bebaf942c8a0450e076bd7846b74ac8d10274
LucaRomano2 <86124841+LucaRomano2@users.noreply.github.com>
2022-03-25 00:34:05 +0100

    Added Island_test, Schoolboard_test

--- src/test/java/it/polimi/ingsw/GameTest.java
+++ src/test/java/it/polimi/ingsw/GameTest.java
@@ -1,8 +1,6 @@
 package it.polimi.ingsw;
 
-import it.polimi.ingsw.board.Color;
-import it.polimi.ingsw.board.Island;
-import it.polimi.ingsw.board.Students;
+import it.polimi.ingsw.board.*;
 import it.polimi.ingsw.exceptions.AssistantAlreadyPlayedException;
 import it.polimi.ingsw.exceptions.EmptyBagException;
 import it.polimi.ingsw.exceptions.EmptyMovableException;
@@ -15,6 +13,8 @@ import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class GameTest {
 
@@ -71,4 +71,53 @@ public class GameTest {
 
         // TODO: not finished
     }
+
+    @Test
+    public void Islandtest(){
+        Island island1 = new Island();
+        Island island2 = new Island();
+
+        Students student1 = new Students(2, 3, 1, 0, 2);
+        Students student2 = new Students(1, 0, 2, 3, 0);
+
+        Player player1 = new Player("Player1", 0);
+        Player player2 = new Player("Player2", 1);
+
+        island1.setMotherNature(true);
+        island1.setStudents(student1);
+        island1.setNumTowers(1);
+        island1.setOwner(player1);
+        island1.setNoEntryTiles(0);
+
+        island2.setMotherNature(false);
+        island2.setStudents(student2);
+        island2.setNumTowers(2);
+        island2.setOwner(player2);
+        island2.setNoEntryTiles(0);
+
+        Students sum_student = new Students(3, 3, 3, 3, 2);
+
+        island1.merge(island2);
+
+        assertEquals(true, island1.hasMotherNature());
+        assertEquals(sum_student, island1.getStudents());
+        assertEquals(3, island1.getNumTowers());
+        assertEquals(player1, island1.getOwner());
+        assertEquals(0, island1.getNoEntryTiles());
+    }
+
+    @Test
+    public void SchoolBoard_test(){
+        Professors professor = new Professors();
+        Students dining_students = new Students(4, 5, 2, 6, 1);
+        Students entrance_students = new Students(2, 2, 1, 3, 1);
+
+        SchoolBoard schoolboard = new SchoolBoard(3, professor, dining_students, entrance_students);
+
+        assertEquals(3, schoolboard.getNumTowers());
+        assertEquals(professor, schoolboard.getProfessors());
+        assertEquals(dining_students, schoolboard.getDiningStudents());
+        assertEquals(entrance_students, schoolboard.getEntranceStudents());
+    }
+
 }
