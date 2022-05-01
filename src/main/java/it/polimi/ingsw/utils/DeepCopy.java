70f4d9151c186de07340b34707acc518200c1cb6
Lorenzo Rossi <30434380+tpoppo@users.noreply.github.com>
2022-05-02 00:37:55 +0200

    Create DeepCopy.java

---
+++ src/main/java/it/polimi/ingsw/utils/DeepCopy.java
@@ -0,0 +1,22 @@
+package it.polimi.ingsw.utils;
+
+import java.io.*;
+
+public class DeepCopy {
+
+    public static Object copy(Serializable object){
+        //Serialization of object
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        try {
+            ObjectOutputStream out = new ObjectOutputStream(bos);
+            out.writeObject(object);
+
+            //De-serialization of object
+            ByteArrayInputStream bis = new ByteArrayInputStream(bos.toByteArray());
+            ObjectInputStream in = new ObjectInputStream(bis);
+            return in.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e); // it is unreachable
+        }
+    }
+}
