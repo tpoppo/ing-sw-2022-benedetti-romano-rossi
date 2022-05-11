273b0fc5b54b25add17628741102a318f1a3891d
LucaRomano2 <86124841+LucaRomano2@users.noreply.github.com>
2022-05-11 19:47:46 +0200

    Updated CLIArt

--- src/main/java/it/polimi/ingsw/view/CLIArt.java
+++ src/main/java/it/polimi/ingsw/view/CLIArt.java
@@ -1,8 +1,10 @@
 package it.polimi.ingsw.view;
 
 import it.polimi.ingsw.client.ClientSocket;
+import it.polimi.ingsw.controller.GameState;
 import it.polimi.ingsw.controller.LobbyHandler;
 import it.polimi.ingsw.controller.LobbyPlayer;
+import it.polimi.ingsw.model.Player;
 import it.polimi.ingsw.model.board.Color;
 import it.polimi.ingsw.model.characters.Character;
 import it.polimi.ingsw.network.messages.*;
@@ -50,7 +52,30 @@ public class CLIArt extends CLI {
     }
 
     private void printState(){
+        Player current_player = model.getCurrentPlayer();
+        print(ansi().bg(Ansi.Color.WHITE).a("Turn: "), 0 , 0);
+        print(ansi().bg(Ansi.Color.WHITE).a(current_player.getUsername()), 0 , 7);
+        GameState gamestate = gameHandler.getCurrentState();
 
+        if(username == current_player.getUsername()){
+            if(gamestate == GameState.PLAY_ASSISTANT){
+                print(ansi().bg(Ansi.Color.WHITE).a("It's your turn, play an assistant card..."), 2 , 0);
+            }
+            if(gamestate == GameState.CHOOSE_CLOUD){
+                print(ansi().bg(Ansi.Color.WHITE).a("It's your turn, choose a cloud..."), 2 , 0);
+            }
+            if(gamestate == GameState.MOVE_MOTHER_NATURE){
+                print(ansi().bg(Ansi.Color.WHITE).a("It's your turn, move mother nature..."), 2 , 0);
+            }
+            if(gamestate == GameState.MOVE_STUDENT){
+                print(ansi().bg(Ansi.Color.WHITE).a("It's your turn, move a student..."), 2 , 0);
+            }
+            if(gamestate == GameState.ACTIVATE_CHARACTER){
+                print(ansi().bg(Ansi.Color.WHITE).a("It's your turn, activate a character..."), 2 , 0);
+            }
+        }else{
+            print(ansi().bg(Ansi.Color.WHITE).a("It's " + current_player + " turn, wait..."), 2 , 0);
+        }
     }
 
     private void printClouds(){
