882b94ee093899e479d6f8b82aa3cebe6539f461
Lorenzo Rossi <30434380+tpoppo@users.noreply.github.com>
2022-05-15 21:09:22 +0200

    Update AppTest.java

--- src/test/java/it/polimi/ingsw/AppTest.java
+++ src/test/java/it/polimi/ingsw/AppTest.java
@@ -1,11 +1,58 @@
 package it.polimi.ingsw;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import it.polimi.ingsw.client.ClientSocket;
+import it.polimi.ingsw.network.Server;
+import it.polimi.ingsw.view.CLI;
+import it.polimi.ingsw.view.CLIArt;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest {
+    @Test
+    void RunServer() throws InterruptedException {
+        runThread(() -> {
+            Server server = Server.getInstance();
+        });
+}
+
+    @Test
+    void RunCli() throws InterruptedException {
+        runThread(() -> {Server server = Server.getInstance();});
+        runThread(() -> {
+            ClientSocket client_socket = new ClientSocket();
+            CLI cli = new CLI(client_socket);
+            cli.run();
+        });
+    }
+
+    @Test
+    void RunCliArt() throws InterruptedException {
+        runThread(() -> {Server server = Server.getInstance();});
+        runThread(() -> {
+            ClientSocket client_socket = new ClientSocket();
+            CLIArt cli = new CLIArt(client_socket);
+            cli.run();
+        });
+    }
+
+    void runThread(Runnable runnable) throws InterruptedException {
+        // this method is required as an exception in the
+        AtomicReference<Exception> exc = new AtomicReference<>();
+        new Thread(() -> {
+            try{
+                runnable.run();
+            } catch (Exception e){
+                exc.set(e);
+            }
+        }).start();
+        Thread.sleep(300);
+        assertNull(exc.get());
+    }
 
 }
