0963048f5df035d010e9c4fd30d2b56ffa8716b6
LucaRomano2 <86124841+LucaRomano2@users.noreply.github.com>
2022-05-31 12:47:41 +0200

    Fixed MenuController

--- src/main/java/it/polimi/ingsw/view/guicontroller/MenuController.java
+++ src/main/java/it/polimi/ingsw/view/guicontroller/MenuController.java
@@ -8,28 +8,24 @@ import it.polimi.ingsw.view.GUI;
 import it.polimi.ingsw.view.viewcontent.ViewContent;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.SplitMenuButton;
 import javafx.scene.control.MenuItem;
 
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
 
-public class MenuController {
+public class MenuController implements Initializable {
 
     @FXML
     private SplitMenuButton choose_lobby;
 
-    public void Create_Lobby_2_Players(ActionEvent event){
-        CreateLobbyMessage createlobbymessage = new CreateLobbyMessage(2);
-        GUI.getClientSocket().send(createlobbymessage);
-    }
-
-    public void Create_Lobby_3_Players(ActionEvent event){
-        CreateLobbyMessage createLobbyMessage = new CreateLobbyMessage(3);
-        GUI.getClientSocket().send(createLobbyMessage);
-    }
-
-    public void Choose_Lobby(ActionEvent event){
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
         ArrayList<MenuItem> menuitem = new ArrayList<>();
         ViewContent view = GUI.getView();
         ArrayList<ReducedLobby> lobbies = view.getLobbies();
@@ -45,10 +41,43 @@ public class MenuController {
 
         for(int i=0; i<menuitem.size(); i++){
             final int pos = i;
-            menuitem.get(i).setOnAction((e)-> {
+            menuitem.get(i).setOnAction((ActionEvent)-> {
                 JoinLobbyMessage joinlobbymessage = new JoinLobbyMessage(lobbies.get(pos).getID());
                 GUI.getClientSocket().send(joinlobbymessage);
             });
         }
     }
+
+    public void Create_Lobby_2_Players(ActionEvent event){
+        CreateLobbyMessage createlobbymessage = new CreateLobbyMessage(2);
+        GUI.getClientSocket().send(createlobbymessage);
+    }
+
+    public void Create_Lobby_3_Players(ActionEvent event){
+        CreateLobbyMessage createLobbyMessage = new CreateLobbyMessage(3);
+        GUI.getClientSocket().send(createLobbyMessage);
+    }
+
+    /*public void Choose_Lobby(ActionEvent event){
+        ArrayList<MenuItem> menuitem = new ArrayList<>();
+        ViewContent view = GUI.getView();
+        ArrayList<ReducedLobby> lobbies = view.getLobbies();
+
+        for(int i=0; i<lobbies.size(); i++){
+            menuitem.add(new MenuItem("Lobby " + lobbies.get(i).getID()));
+            menuitem.get(i).setText("Lobby " + lobbies.get(i).getID());
+        }
+
+        for(int i=0; i<menuitem.size(); i++){
+            choose_lobby.getItems().add(menuitem.get(i));
+        }
+
+        for(int i=0; i<menuitem.size(); i++){
+            final int pos = i;
+            menuitem.get(i).setOnAction((ActionEvent)-> {
+                JoinLobbyMessage joinlobbymessage = new JoinLobbyMessage(lobbies.get(pos).getID());
+                GUI.getClientSocket().send(joinlobbymessage);
+            });
+        }
+    }*/
 }
