1020621f698e6ed2227ed6896eae9e1b6560391f
Lorenzo Rossi <30434380+tpoppo@users.noreply.github.com>
2022-05-31 16:25:59 +0200

    [GUI] skeleton gamecontroller

--- src/main/java/it/polimi/ingsw/view/guicontroller/GameController.java
+++ src/main/java/it/polimi/ingsw/view/guicontroller/GameController.java
@@ -1,4 +1,18 @@
 package it.polimi.ingsw.view.guicontroller;
 
-public class GameController {
+import it.polimi.ingsw.view.GUI;
+import it.polimi.ingsw.view.viewcontent.ViewContent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GameController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ViewContent view = GUI.getView();
+        System.out.println(view);
+    }
 }
