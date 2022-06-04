9b6100b0dbaef2d4935e62a877a5acdc7889c09e
Benedart <49732385+Benedart@users.noreply.github.com>
2022-06-04 16:38:36 +0200

    [GUI] Optimized scene loading
    Updated switchScene method

--- src/main/java/it/polimi/ingsw/view/GUI.java
+++ src/main/java/it/polimi/ingsw/view/GUI.java
@@ -1,12 +1,12 @@
 package it.polimi.ingsw.view;
 
 import it.polimi.ingsw.client.ClientSocket;
+import it.polimi.ingsw.model.Player;
+import it.polimi.ingsw.view.guicontroller.GUIController;
 import it.polimi.ingsw.view.viewcontent.ViewContent;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.CacheHint;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
@@ -16,6 +16,12 @@ import javafx.stage.Stage;
 
 import java.awt.*;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class GUI extends Application {
     private static ViewContent view;
@@ -24,23 +30,62 @@ public class GUI extends Application {
     private static Stage stage;
     private static boolean creatingLobby;
     private static Integer selectingCharacter;
+    public static Player schoolboardPlayer;
+    private final Logger LOGGER = Logger.getLogger(getClass().getName());
 
+    private final HashMap<String, Scene> sceneMap = new HashMap<>();
+    private static final String LOGIN = "/fxml/login.fxml";
+    private static final String MENU = "/fxml/menu.fxml";
+    private static final String LOBBY = "/fxml/lobby.fxml";
+    private static final String GAME = "/fxml/game.fxml";
+
+    private final HashMap<Scene, GUIController> controllerMap = new HashMap<>();
 
     public static void main(String[] args) {
    //     System.setProperty("prism.allowhidpi", "false");
         launch(args);
     }
 
+    public void setup() {
+        List<String> fxmlList = new ArrayList<>(Arrays.asList(LOGIN, MENU, LOBBY, GAME));
+        try {
+            for (String path : fxmlList) {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource(path));
+                Scene scene = new Scene(loader.load());
+                sceneMap.put(path, scene);
+                GUIController controller = loader.getController();
+                controllerMap.put(scene, controller);
+            }
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, e.getMessage(), e);
+            e.printStackTrace();
+        }
+    }
+
+    private void resizeScenes(List<String> scenePaths, int width, int height){
+        for(String scenePath : scenePaths){
+            Scene sceneToResize = sceneMap.get(scenePath);
+            sceneMap.replace(scenePath, resizeScene(sceneToResize, width, height));
+        }
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
+        setup();
+
+        resizeScenes(List.of(GAME), 1920, 1080);
+
         GUI.stage = stage;
         stage.setTitle("Eriantys");
         stage.getIcons().add(new Image(GUI.class.getResourceAsStream("/graphics/other/coin.png")));
         stage.setFullScreenExitHint("");
         stage.setResizable(false);
 
-        switchScene("/fxml/login.fxml");
-        // stage.getScene().getStylesheets().add("css/login.css");
+        Scene scene = sceneMap.get(LOGIN);
+        scene.getStylesheets().add("css/login.css");
+
+        stage.setScene(scene);
+        stage.show();
 
         startViewContentUpdates(stage);
         stage.setOnCloseRequest(event -> {
@@ -64,11 +109,10 @@ public class GUI extends Application {
 
                 GUI.view = client_socket.getView();
 
-
                 if (view.getCurrentHandler() == null) { // show the menu
                     Platform.runLater(() -> {
                         try {
-                            switchScene("/fxml/menu.fxml");
+                            switchScene(sceneMap.get(MENU));
                             stage.getScene().getStylesheets().add("css/menu.css");
                             stage.setFullScreen(false);
                         } catch (IOException e) {
@@ -77,11 +121,22 @@ public class GUI extends Application {
                     });
                 } else {
                     switch (view.getCurrentHandler()) {
+                        case LOBBY -> // show the lobby
+                                Platform.runLater(() -> {
+                                    try {
+                                        switchScene(sceneMap.get(LOBBY));
+                                        stage.getScene().getStylesheets().add("css/lobby.css");
+                                        stage.setFullScreen(false);
+                                    } catch (IOException e) {
+                                        throw new RuntimeException(e);
+                                    }
+                                });
+
                         case GAME -> // show the game
                                 Platform.runLater(() -> {
                                     try {
                                         long startTime = System.currentTimeMillis();
-                                        switchSceneResize("/fxml/game.fxml", 1920, 1080);
+                                        switchScene(sceneMap.get(GAME));
                                         stage.getScene().getStylesheets().add("css/game.css");
                                         stage.setFullScreen(true);
                                         long estimatedTime = System.currentTimeMillis() - startTime;
@@ -90,18 +145,6 @@ public class GUI extends Application {
                                         throw new RuntimeException(e);
                                     }
                                 });
-
-                        case LOBBY -> // show the lobby
-                                Platform.runLater(() -> {
-                                    try {
-                                        switchScene("/fxml/lobby.fxml");
-                                        stage.getScene().getStylesheets().add("css/lobby.css");
-                                        stage.setFullScreen(false);
-                                    } catch (IOException e) {
-                                        throw new RuntimeException(e);
-                                    }
-                                });
-
                     }
                 }
 
@@ -132,32 +175,23 @@ public class GUI extends Application {
         }
     }
 
-    public static void switchScene(String scenePath) throws IOException{
-        Parent root = FXMLLoader.load(ResourcesCache.getResource(scenePath));
-        Scene scene = new Scene(root);
-
-        scene.getRoot().setCacheHint(CacheHint.SPEED);
-        scene.getRoot().setCache(true);
+    public void switchScene(Scene scene) throws IOException{
         stage.setScene(scene);
+        controllerMap.get(scene).setup();
+
         stage.show();
     }
 
-    public static void switchSceneResize(String scenePath, int width, int heigth) throws IOException {
-        Parent root = FXMLLoader.load(ResourcesCache.getResource(scenePath));
-        Scene scene = new Scene(root);
-
+    public Scene resizeScene(Scene scene, int width, int height) {
         Dimension resolution = Toolkit.getDefaultToolkit().getScreenSize();
         double currentWidth = resolution.getWidth();
         double currentHeight = resolution.getHeight();
         double w = currentWidth/width;
-        double h = currentHeight/heigth;
+        double h = currentHeight/height;
         Scale scale = new Scale(w, h, 0, 0);
-        root.getTransforms().add(scale);
+        scene.getRoot().getTransforms().add(scale);
 
-        scene.getRoot().setCache(true);
-        scene.getRoot().setCacheHint(CacheHint.SPEED);
-        stage.setScene(scene);
-        stage.show();
+        return scene;
     }
 
     public static String getUsername() {
@@ -179,8 +213,6 @@ public class GUI extends Application {
     public static ViewContent getView() {
         return view;
     }
-
-    public static Stage getStage() {return stage;}
 
     public static boolean isCreatingLobby() {
         return creatingLobby;
